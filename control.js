d14c4113c21d0542665296bbb4b78f14f2a30d37
Wireless Power Squid <sp899@njit.edu>
2015-10-09 01:01:15 +0000

    Reused loop index variable.

--- control.js
+++ control.js
@@ -1,14 +1,15 @@
 webiopi().ready(function() {
 var gpioPins = [2,3,4,17,27,22,10,9];
 
-for(var i=0; i<gpioPins.length; i++) {
+for(i=0; i<gpioPins.length; i++) {
 	webiopi().setFunction(gpioPins[i], "out");
 }
 
 	var content, button;
 	content = $("#content");
-for(var j=1; j<=gpioPins.length; j++) {
-	button = webiopi().createGPIOButton(gpioPins[j], "Relay " + j);
+for(i=0; i<gpioPins.length; i++) {
+	label = "Relay ";
+	button = webiopi().createGPIOButton(gpioPins[i], label.concat(i+1));
 	content.append(button); // append button to content div
 }
        
